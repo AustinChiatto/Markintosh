4e9afd60c165d7097134edd19022fec291bb8cba
Austin Chiatto <chiattoaustin@gmail.com>
2024-11-10 22:39:47 -0800

    Initial Commit

---
+++ MdNotes/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  MdNotes
+//
+//  Created by Austin Chiatto on 2024-11-10.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
